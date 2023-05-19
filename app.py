a4c56dda051be3c50b16f07b6ff820a177a625a7
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-05-19 21:46:58 +0900

    Add rinna GPT NeoX

--- app.py
+++ app.py
@@ -4,6 +4,58 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import streamlit as st
 
 
+def generate_text_by_open_calm(input_text: str) -> str:
+
+    model_name = "cyberagent/open-calm-small"
+
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                 device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # if torch.backends.mps.is_available():
+    #     device = torch.device('mps')
+    #     model.to(device)
+
+    inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+    with torch.no_grad():
+        tokens = model.generate(
+            **inputs,
+            max_new_tokens=64,
+            do_sample=True,
+            temperature=0.7,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+
+    output = tokenizer.decode(tokens[0], skip_special_tokens=True)
+    return output
+
+
+def generate_text_by_rinna_gpt_neox(input_text: str) -> str:
+    model_name = "rinna/japanese-gpt-neox-small"
+
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                 device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_name,
+                                              use_fast=False)
+
+    # if torch.backends.mps.is_available():
+    #     device = torch.device('mps')
+    #     model.to(device)
+
+    inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+    with torch.no_grad():
+        tokens = model.generate(
+            **inputs,
+            max_new_tokens=64,
+            do_sample=True,
+            temperature=0.7,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+
+    output = tokenizer.decode(tokens[0], skip_special_tokens=True)
+    return output
+
+
 if __name__ == "__main__":
     st.title("JPN LLM")
 
@@ -23,23 +75,13 @@ if __name__ == "__main__":
     input_text = st.text_input("input:")
 
     if input_text != "":
-        model_name = model_types[model_names.index(selected_model)]
+        output = ""
 
-        model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                     device_map="auto")
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        if selected_model == model_names[0]:
+            output = generate_text_by_open_calm(input_text)
+        elif selected_model == model_names[1]:
+            output = generate_text_by_rinna_gpt_neox(input_text)
 
-        inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
-        with torch.no_grad():
-            tokens = model.generate(
-                **inputs,
-                max_new_tokens=64,
-                do_sample=True,
-                temperature=0.7,
-                pad_token_id=tokenizer.pad_token_id,
-            )
-
-        output = tokenizer.decode(tokens[0], skip_special_tokens=True)
         print(output)
 
         st.text_area(f"output by {selected_model}:", output, height=300)
